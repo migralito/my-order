b8107c0a640bee4cd282836d9d61c1d3d5164701
aleWidomlanski <widomlanski@hotmail.com>
2022-02-23 16:59:08 -0300

    se optimizo input

--- src/components/Input.js
+++ src/components/Input.js
@@ -9,38 +9,22 @@ const Input = ({ tableNumber, setTableNumber }) => {
         setTableNumber(e.target.value)
     }
 
-
-    if (tableNumber === "") {
-        return (
-            <div className={styles.container}>
-                <Description description={'Por favor ingresá tu número de mesa.'} />
-                <div className={styles.inputLink}>
-                    <input onChange={handleClick} value={tableNumber} placeholder={"N° Mesa"} className={styles.input} />
-                    <Link to={`/`} className={styles.enter}>Entrar</Link>
-                </div>
+    return (
+        <div className={styles.container}>
+            <Description description={'Por favor ingresá tu número de mesa.'} />
+            <div className={tableNumber === ""  ? styles.inputLink : (parseInt(tableNumber) > 50 || isNaN(parseInt(tableNumber)) || parseInt(tableNumber[0]) === 0) ? styles.inputLinkError : styles.inputLink}>
+                <input onChange={handleClick} value={tableNumber} placeholder={"N° Mesa"} className={styles.input} />
+                {
+                    tableNumber === "" ? <Link to={`/`} className={styles.enter}>Entrar</Link> :
+                        (parseInt(tableNumber) > 50 || isNaN(parseInt(tableNumber)) || parseInt(tableNumber[0]) === 0) ?
+                            <Description description={'Debe escribir un número de mesa existente.'} clases={styles.error} /> :
+                            <Link to={`table/${tableNumber}`} className={styles.enter}>Entrar</Link>
+                }
             </div>
-        )
-    } else if (parseInt(tableNumber) > 50 || isNaN(parseInt(tableNumber)) || parseInt(tableNumber[0]) === 0) {
-        return (
-            <div className={styles.container}>
-                <Description description={'Por favor ingresá tu número de mesa.'} />
-                <div className={styles.inputLinkError}>
-                    <input onChange={handleClick} value={tableNumber} placeholder={"N° Mesa"} className={styles.input} />
-                    <Description description={'Debe escribir un número de mesa existente.'} clases={styles.error} />
-                </div>
-            </div>
-        )
-    } else {
-        return (
-            <div className={styles.container}>
-                <Description description={'Por favor ingresá tu número de mesa.'} />
-                <div className={styles.inputLink}>
-                    <input onChange={handleClick} value={tableNumber} placeholder={"N° Mesa"} className={styles.input} />
-                    <Link to={`table/${tableNumber}`} className={styles.enter}>Entrar</Link>
-                </div>
-            </div>
-        )
-    }
+        </div>
+    )
 };
 
 export default Input;
+
+
