e62d9891500cd058fd830de648f5e21875c78527
Alejandro Widomlanski <widomlanski@hotmail.com>
2022-02-26 10:45:47 -0300

    prueba-sin-amplify

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
-import Amplify from "aws-amplify";
+/* import Amplify from "aws-amplify";
 import awsExports from "./aws-exports";
-Amplify.configure(awsExports);
+Amplify.configure(awsExports); */
 
 
 ReactDOM.render(
