b6479464ff9fbf8b68b5fb439b1f9f9f72e6cf27
Alejandro Widomlanski <widomlanski@hotmail.com>
2022-02-25 22:00:08 -0300

    amplify

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
 /* import reportWebVitals from './reportWebVitals'; */
 
 ReactDOM.render(
