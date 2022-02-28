a0dbe03ff408a578c6d39c3a3195b29750f79d5b
Alejandro Widomlanski <widomlanski@hotmail.com>
2022-02-28 19:34:30 -0300

    cambio-index-sector-amplify

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
-import Amplify from "aws-amplify";
+/* import Amplify from "aws-amplify";
 import awsExports from "./aws-exports";
-Amplify.configure(awsExports);
+Amplify.configure(awsExports); */
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
 
 
 ReactDOM.render(
