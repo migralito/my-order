479b34e75dbc98fe701b149708932e208128fde9
Alejandro Widomlanski <widomlanski@hotmail.com>
2022-02-28 19:40:46 -0300

    cambio-index-original-amplify

--- src/index.js
+++ src/index.js
@@ -3,12 +3,10 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
-/* import Amplify from "aws-amplify";
+import Amplify from "aws-amplify";
 import awsExports from "./aws-exports";
-Amplify.configure(awsExports); */
-import Amplify from 'aws-amplify';
-import config from './aws-exports';
-Amplify.configure(config);
+Amplify.configure(awsExports);
+
 
 
 ReactDOM.render(
